370128f1cd5d1b52c2a1ebaf626c26c111a98935
yash <yashwable12@gmail.com>
2022-11-22 17:57:49 +0530

    priority

---
+++ spu scheduling/priority.java
@@ -0,0 +1,100 @@
+import java.util.*;
+
+public class priority {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter no of processes");
+        int n = sc.nextInt();
+        int[] pid = new int[n];
+        int[] at = new int[n];
+        int[] bt = new int[n];
+        int[] priority = new int[n];
+        int[] wt = new int[n];
+        int[] tat = new int[n];
+        int[] ct = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("enter arrival time of process" + (i + 1));
+            at[i] = sc.nextInt();
+            System.out.println("enter burst time of process" + (i + 1));
+            bt[i] = sc.nextInt();
+            System.out.println("enter priority of process" + (i + 1));
+            priority[i] = sc.nextInt();
+            pid[i] = i + 1;
+        }
+        int temp;
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (at[j] > at[j + 1]) {
+                    temp = at[j];
+                    at[j] = at[j + 1];
+                    at[j + 1] = temp;
+
+                    temp = bt[j];
+                    bt[j] = bt[j + 1];
+                    bt[j + 1] = temp;
+
+                    temp = priority[j];
+                    priority[j] = priority[j + 1];
+                    priority[j + 1] = temp;
+
+                    temp = pid[j];
+                    pid[j] = pid[j + 1];
+                    pid[j + 1] = temp;
+                }
+                if (at[j] == at[j + 1]) {
+                    if (priority[j] > priority[j + 1]) {
+                        temp = at[j];
+                        at[j] = at[j + 1];
+                        at[j + 1] = temp;
+
+                        temp = bt[j];
+                        bt[j] = bt[j + 1];
+                        bt[j + 1] = temp;
+
+                        temp = priority[j];
+                        priority[j] = priority[j + 1];
+                        priority[j + 1] = temp;
+
+                        temp = pid[j];
+                        pid[j] = pid[j + 1];
+                        pid[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        ct[0] = at[0] + bt[0];
+        tat[0] = ct[0] - at[0];
+        wt[0] = tat[0] - bt[0];
+
+        for (int i = 1; i < n; i++) {
+            ct[i] = ct[i - 1] + bt[i];
+            tat[i] = ct[i] - at[i];
+            wt[i] = tat[i] - bt[i];
+        }
+
+        float sumwt = 0, sumtat = 0;
+
+        for (int i = 0; i < n; i++) {
+            sumwt += wt[i];
+            sumtat += tat[i];
+        }
+
+        float avgwt = sumwt / n;
+        float avgtat = sumtat / n;
+
+        System.out.println("Priority Schedulig algorithm");
+        System.out.format("%20s%20s%20s%20s%20s%20s%20s\n", "pid", "arrivalTime", "burstTime", "priority",
+                "waitingTime", "TurnAroundTime", "completionTime");
+
+        for (int i = 0; i < n; i++) {
+            System.out.format("%20d%20d%20d%20d%20d%20d%20d\n", pid[i], at[i], bt[i], priority[i], wt[i], tat[i],
+                    ct[i]);
+        }
+        System.out.format("%80s%20f%20f", "average", avgwt, avgtat);
+        sc.close();
+
+    }
+}
